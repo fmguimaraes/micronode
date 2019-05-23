3718ff5d81b6aa0e63ad15f7460883375eef09d7
root <root@ubuntu.home>
2019-05-23 06:46:46 +0200

    docker support

--- M2Object.js
+++ M2Object.js
@@ -4,6 +4,7 @@ class M2Object {
   constructor (app) {
     this.app = app;
     if(!!app) {
+      this.docker = app.docker;
       this.log = app.log;
       this.socket = app.socket;
       this.httpServer = app.httpServer;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 "use strict"
+let Docker = require('dockerode');
+const Settings = require('../settings');
 let HTTPServer = require('./utils/httpServer.js');
 let RESTFacade = require('./routes/restFacade.js');
 let Socket = require('./utils/socket.js');
 let Actions = require('../actions/actions.js');
 let EventDispatcher = require('./utils/EventDispatcher.js');
 let Log = require('./utils/Log.js');
-
 class ApplicationService {
   constructor() {
+    this.docker = new Docker({socketPath: Settings.Servers.docker || '/var/run/docker.sock'});
     this.httpServer = new HTTPServer(this);
     this.log = new Log(this);
     this.eventEmitter = new EventDispatcher(this);
@@ -15,7 +17,7 @@ class ApplicationService {
 
     this.rest =  new RESTFacade(this);
     this.actions =  new Actions(this);
-  
+
     this.httpServer.start(); 
   }
 }
