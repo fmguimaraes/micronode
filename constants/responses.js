28978a27e09d179806c19529fb74ea90d13dc3f5
Romain Duret <Romain.duret@haliodx.com>
2019-06-05 11:36:13 +0200

    feat: new answer for annotations

--- constants/responses.js
+++ constants/responses.js
@@ -5,7 +5,11 @@ const responses = {
     UNABLE_TO_CREATE_USER: { message: "unable to create user" },
     INVALID_PASSWORD: { error: { code:401, message: "invalid username or password"} },
     TOKEN_NOT_PROVIDED: { message: "token not provided", auth: false, token: null },
-    INVALID_TOKEN: { message: "invalid token", auth: false, token: null }
+    INVALID_TOKEN: { message: "invalid token", auth: false, token: null },
+
+    ANNOTATION_ALREADY_EXISTS: { message: "annotation already exists"},
+    UNABLE_TO_CREATE_ANNOTATION: { message: "unable to create annotation"},
+    
 };
 
 module.exports = responses;
